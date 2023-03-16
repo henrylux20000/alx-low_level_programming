659fdb22b3f88ea6c386cd5cb2dbc1a776aeb096
henrylux20000 <henrylux2000@yahoo.com>
2023-03-16 16:05:43 -0700

    update print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+ * Program to print hello
+ */
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art");
- return (0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }

bf3d4e73deeeca543bf27c119a27730d0383fdc3
henrylux20000 <henrylux2000@yahoo.com>
2023-03-19 06:41:26 -0700

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+int a = 0;
+for (; a <= 9; a++)
+{
+printf("%d", a);
+}
+return (0);
+}

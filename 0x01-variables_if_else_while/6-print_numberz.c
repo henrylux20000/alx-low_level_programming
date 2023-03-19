0f5cca7ed12fe2ccdb106f9aa92b1f1bc72ac8a3
henrylux20000 <henrylux2000@yahoo.com>
2023-03-19 11:20:53 -0700

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int a = 0 + '0';
+while (a <= 9 + '0')
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}

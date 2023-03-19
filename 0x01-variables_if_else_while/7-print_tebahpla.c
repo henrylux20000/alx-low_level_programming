1350d65ae97462db0cbfc7d96338247bd5b5bf8d
henrylux20000 <henrylux2000@yahoo.com>
2023-03-19 15:32:47 -0700

    z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char a = 'z';
+while (a >= 'a')
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}

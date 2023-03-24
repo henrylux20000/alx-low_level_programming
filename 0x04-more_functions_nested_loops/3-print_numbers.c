98b94239456927d1292227547c338740aecf9344
henrylux20000 <henrylux2000@yahoo.com>
2023-03-24 03:25:15 -0700

    change print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_numbers - print number 1-9
  */
 void print_numbers(void)
 {
-int a = 0;
-while (a < 9)
+int a = 0 + '0';
+while (a < 9 + '0')
 {
-printf("%d", a);
+_putchar(a);
 a++;
 }
+_putchar('\n');
 }

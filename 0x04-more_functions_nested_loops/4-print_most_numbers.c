83704c3ae588c80ce491975f9a14d7eef8c96f00
henrylux20000 <henrylux2000@yahoo.com>
2023-03-24 11:34:40 -0700

    update most

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * print_most_numbers - 2 and 4
  */
@@ -8,11 +8,13 @@ void print_most_numbers(void)
 int a = 0 + '0';
 while (a <= 9 + '0')
 {
-if (a == 2 + '0'|| a == 4 + '0')
+if (a == 2 + '0' || a == 4 + '0')
 {
 continue;
 a++;
 }
 _putchar(a);
-a++;}
+a++;
+}
+_putchar('\n');
 }

d21b37a3e292bddfe7272facb4741cf46714e842
henrylux20000 <henrylux2000@yahoo.com>
2023-03-25 04:04:14 -0700

    print a-z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet
+ * print_alphabet - print letter A-z
  */
 void print_alphabet(void)
 {
 char a;
 print_alphabet();
 for (a = 'a'; a <= 'z'; a++)
-_putchar(a);
+{_putchar(a);
+}
 _putchar('\n');
 }
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * main - check the code
  *
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    print_alphabet();
-    return (0);
+print_alphabet();
+return (0);
 }
